8d6c2d02435fd0b6cd2cdb15535a650a2e02667e
Kate <polyakovkate@gmail.com>
2024-01-02 14:13:37 -0800

    trying delete node

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 import express from 'express';
 import jwt from 'jsonwebtoken';
 import mongoose from 'mongoose';
+import { validationResult } from 'express-validator';
+
+import { registerValidation } from './validations/auth.js';
+import UserModel from './models/user.js';
 
 mongoose
   .connect('mongodb+srv://admin:123@blog-mern.34ltnbv.mongodb.net/?retryWrites=true&w=majority')
@@ -10,8 +14,22 @@ mongoose
 const app = express();
 app.use(express.json());
 
-app.post('/auth/register', (req, res) => {
-  
+app.post('/auth/register', registerValidation, (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json(errors.array());
+  }
+
+  const doc = new UserModel({
+    email: req.body.email,
+    fullName: req.body.fullName,
+    avatarUrl: req.body.avatarUrl,
+    passwordHash: req.body.passwordHash,
+  })
+
+  res.json({
+    success: true,
+  });
 });
 
 app.listen(4444, (err) => {
