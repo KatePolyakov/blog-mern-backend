121876fedf3291ab2c317ec8a2927c5c16241b04
Kate <polyakovkate@gmail.com>
2024-01-02 21:16:46 -0800

    created middleware for auth and refactored UserController

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 import express from 'express';
-import jwt from 'jsonwebtoken';
-import bcrypt from 'bcrypt';
 import mongoose from 'mongoose';
-import { validationResult } from 'express-validator';
+
 
 import { registerValidation } from './validations/auth.js';
-import UserModel from './models/user.js';
+import checkAuth from './utils/checkAuth.js';
+import { register, login, getMe } from './controllers/UserController.js';
 
 mongoose
   .connect('mongodb+srv://admin:123@blog-mern.34ltnbv.mongodb.net/blog?retryWrites=true&w=majority')
@@ -15,91 +14,11 @@ mongoose
 const app = express();
 app.use(express.json());
 
-app.post('/auth/login', async (req, res) => {
-  try {
-    const user = await UserModel.findOne({ email: req.body.email });
+app.post('/auth/login', login);
 
-    if (!user) {
-      return res.status(404).json({
-        message: 'Login/password not found',
-      });
-    }
+app.post('/auth/register', registerValidation, register);
 
-    const isValidPass = await bcrypt.compare(req.body.password, user._doc.passwordHash);
-
-    if (!isValidPass) {
-      return res.status(404).json({
-        message: 'Login/password not found',
-      });
-    }
-
-    const token = jwt.sign(
-      {
-        _id: user._id,
-      },
-      'secret123',
-      {
-        expiresIn: '30d',
-      },
-    );
-
-    const { passwordHash, ...userData } = user._doc;
-
-    res.json({
-      ...userData,
-      token,
-    });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({
-      message: 'Registration Error',
-    });
-  }
-});
-
-app.post('/auth/register', registerValidation, async (req, res) => {
-  try {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json(errors.array());
-    }
-
-    const password = req.body.password;
-    const salt = await bcrypt.genSalt(10);
-    const hash = await bcrypt.hash(password, salt);
-
-    const doc = new UserModel({
-      email: req.body.email,
-      fullName: req.body.fullName,
-      avatarUrl: req.body.avatarUrl,
-      passwordHash: hash,
-    });
-
-    const user = await doc.save();
-
-    const token = jwt.sign(
-      {
-        _id: user._id,
-      },
-      'secret123',
-      {
-        expiresIn: '30d',
-      },
-    );
-
-    const { passwordHash, ...userData } = user._doc;
-
-    res.json({
-      ...userData,
-      token,
-    });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({
-      message: 'Registration Error',
-    });
-  }
-});
+app.get('/auth/me', checkAuth, getMe);
 
 app.listen(4444, (err) => {
   if (err) {
